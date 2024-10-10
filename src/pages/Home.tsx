f56d617bb44e6f56ff60b0d74b1afab1daa8b018
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-10 09:09:45 -0700

    added accessibility to hero Links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ export const Home = () => {
             <section className="bg-[url('/src/assets/images/hero-bg.webp')] bg-cover bg-center bg-no-repeat h-[870px] mt-16">
                 <div className="flex flex-col justify-center h-[85%] max-w-[85%] items-stretch p-5 mx-[144px] text-left">
                     <div>
-                        <Link to='/menu'>
-                            <img src={flannel} className='w-[40%] h-auto' decoding='async'/>
+                        <Link to='/menu' aria-label='redirect to menu page'>
+                            <img src={flannel} className='w-[40%] h-auto' alt="Image reading In Fall We Wear Flannel" decoding='async'/>
                         </Link>
                     </div>
                     <div className='max-w-[550px] text-center ml-4'>
@@ -19,8 +19,8 @@ export const Home = () => {
                                 flannel and indulge in all the fall flavors.
                         </p>
                         <div className='mt-16'>
-                            <Link to='/menu' 
-                            className='text-white font-bold bg-[#FB8B00] border-2 border-[#C65301] min-w-[200px] mx-4 mb-4 px-10 py-4 text-center uppercase rounded-full  transition-all duration-200 ease-out hero-btn-hover'>
+                            <Link to='/menu' aria-label='redirect to menu page' 
+                            className='text-white font-bold bg-[#fb8b00] border-2 border-[#C65301] min-w-[200px] mx-4 mb-4 px-10 py-4 text-center uppercase rounded-full  transition-all duration-200 ease-out hero-btn-hover'>
                                 view menu
                             </Link>
                         </div>
