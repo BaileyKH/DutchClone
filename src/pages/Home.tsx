14fa29d13b41b9c293099ea2162c13104ad3f672
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-10 08:43:52 -0700

    Added hero contents

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,33 @@
+import { Link } from 'react-router-dom';
 
+import flannel from '/src/assets/images/hero-flannel.webp'
 
 export const Home = () => {
     return(
         <main>
             <section className="bg-[url('/src/assets/images/hero-bg.webp')] bg-cover bg-center bg-no-repeat h-[870px] mt-16">
+                <div className="flex flex-col justify-center h-[85%] max-w-[85%] items-stretch p-5 mx-[144px] text-left">
+                    <div>
+                        <Link to='/menu'>
+                            <img src={flannel} className='w-[40%] h-auto' decoding='async'/>
+                        </Link>
+                    </div>
+                    <div className='max-w-[550px] text-center ml-4'>
+                        <p className='my-6 font-medium text-lg text-center hero-txt leading-8 text-white'>
+                                Caramel Pumpkin Brûlée is back and brought <br></br>
+                                two new sweet treats! Grab your coziest <br></br>
+                                flannel and indulge in all the fall flavors.
+                        </p>
+                        <div className='mt-16'>
+                            <Link to='/menu' 
+                            className='text-white font-bold bg-[#FB8B00] border-2 border-[#C65301] min-w-[200px] mx-4 mb-4 px-10 py-4 text-center uppercase rounded-full  transition-all duration-200 ease-out hero-btn-hover'>
+                                view menu
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
 }
+
