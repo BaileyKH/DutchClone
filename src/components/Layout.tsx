5c8517810289e7e012ded172c39474fc917dd889
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-11 16:13:58 -0700

    added footer to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
+import { Footer } from "./Footer";
 
 export const Layout = () => {
     return(
         <>
             <NavBar />
             <Outlet />
+            <Footer />
         </>
     );
 }
