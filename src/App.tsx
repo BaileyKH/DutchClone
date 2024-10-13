f48d172514546a41cdedf7fe5b5dd16b42c9ecb7
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-13 16:24:13 -0700

    added placeholder redirect route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Layout } from "./components/Layout"
 import { Home } from "./pages/Home"
+import { InProgress } from "./pages/InProgress"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='/inprogress' element={<InProgress />} />
         </Route>
       </Routes>
     </Router>
