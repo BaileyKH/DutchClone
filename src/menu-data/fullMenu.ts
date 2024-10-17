8b7d87d3f2ef9a052a225e0d3b792b07f48a4393
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-17 09:17:00 -0700

    created drink type data for menu

---
+++ src/menu-data/fullMenu.ts
@@ -0,0 +1,129 @@
+import fallFaves from '/src/assets/images/menu/fullMenu/fallfaves.webp'
+import dutchFaves from '/src/assets/images/menu/fullMenu/dutchFaves.png'
+import protienCoffee from '/src/assets/images/menu/fullMenu/proteincoffee.png'
+import blended from '/src/assets/images/menu/fullMenu/blended.webp'
+import classics from '/src/assets/images/menu/fullMenu/coffee_classics.webp'
+import rebel from '/src/assets/images/menu/fullMenu/rebel.png'
+import lemonade from '/src/assets/images/menu/fullMenu/lemonade.png'
+import icedTea from '/src/assets/images/menu/fullMenu/icedTeam.png'
+import chaiLatte from '/src/assets/images/menu/fullMenu/chaiLatte.webp'
+import shakes from '/src/assets/images/menu/fullMenu/shakes.webp'
+import hotCocoa from '/src/assets/images/menu/fullMenu/hotCocoa.webp'
+import smoothies from '/src/assets/images/menu/fullMenu/smoothies.webp'
+import sparklingSoda from '/src/assets/images/menu/fullMenu/sparklingSoda.png'
+import snacks from '/src/assets/images/menu/fullMenu/snacks.webp'
+
+export const fullMenuData = [
+    {
+        id: 1,
+        menuItem: 'Seasonal Drinks',
+        image: fallFaves,
+        alt: 'dutch bros seasonal drinks',
+        ariaLabel: 'redirect to dutch bros seasonal drinks'
+
+    },
+    {
+        id: 2,
+        menuItem: 'Dutch Faves',
+        image: dutchFaves,
+        alt: 'dutch bros favorites',
+        ariaLabel: 'redirect to dutch bros favorite drinks'
+
+    },
+    {
+        id: 3,
+        menuItem: 'Protein Coffee',
+        image: protienCoffee,
+        alt: 'dutch bros protein coffee',
+        ariaLabel: 'redirect to dutch bros protein coffee drinks'
+
+    },
+    {
+        id: 4,
+        menuItem: 'Blended Freeze',
+        image: blended,
+        alt: 'dutch bros blended drinks',
+        ariaLabel: 'redirect to dutch bros blended drinks'
+
+    },
+    {
+        id: 5,
+        menuItem: 'Coffee Classics',
+        image: classics,
+        alt: 'dutch bros classic coffees',
+        ariaLabel: 'redirect to dutch bros coffee classic drinks'
+
+    },
+    {
+        id: 6,
+        menuItem: 'Dutch Bros Rebel™ Energy Drink',
+        image: rebel,
+        alt: 'dutch bros rebel drinks',
+        ariaLabel: 'redirect to dutch bros rebel drinks'
+
+    },
+    {
+        id: 7,
+        menuItem: 'Lemonade',
+        image: lemonade,
+        alt: 'dutch bros lemonade drinks',
+        ariaLabel: 'redirect to dutch bros lemonade drinks'
+
+    },
+    {
+        id: 8,
+        menuItem: 'Iced Tea',
+        image: icedTea,
+        alt: 'dutch bros iced tea',
+        ariaLabel: 'redirect to dutch bros iced teas'
+
+    },
+    {
+        id: 9,
+        menuItem: 'Chai Latte',
+        image: chaiLatte,
+        alt: 'dutch bros chai lattes',
+        ariaLabel: 'redirect to chai lattes'
+
+    },
+    {
+        id: 10,
+        menuItem: 'Shakes',
+        image: shakes,
+        alt: 'dutch bro shakes',
+        ariaLabel: 'redirec to dutch bros shakes'
+
+    },
+    {
+        id: 11,
+        menuItem: 'Hot Cocoa',
+        image: hotCocoa,
+        alt: 'dutch bros hot cocoa',
+        ariaLabel: 'redirect to dutch bros hot cocoa'
+
+    },
+    {
+        id: 12,
+        menuItem: 'Smoothies',
+        image: smoothies,
+        alt: 'dutch bros smoothies',
+        ariaLabel: 'redirect to dutch bros smoothes'
+
+    },
+    {
+        id: 13,
+        menuItem: 'Sparkling Soda',
+        image: sparklingSoda,
+        alt: 'dutch bros sparkling soda',
+        ariaLabel: 'redirect to dutch bros sparkling sodas'
+
+    },
+    {
+        id: 14,
+        menuItem: 'Snacks',
+        image: snacks,
+        alt: 'dutch bros snacks',
+        ariaLabel: 'redirect to dutch bros snacks'
+
+    },
+]
