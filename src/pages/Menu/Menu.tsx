7fd84dbd7e6f15a0bd3beced6fe6c645d3c29b1d
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-17 11:23:28 -0700

    added menu content and styles

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -8,33 +8,32 @@ import { fullMenuData as menu } from '../../menu-data/fullMenu';
 export const Menu = () => {
     return(
         <main>
-            <div className="flex pt-16 mt-16 w-full">
-                <div className="flex justify-between items-center mb-40 w-full max-w-[1440px] mx-28">
+            <div className="flex justify-center items-center pt-16 mt-16 w-full">
+                <div className="flex flex-col lg:flex-row justify-between items-center mb-16 lg:mb-24 w-full max-w-[1440px] mx-28">
                     <div>
-                        <img src={witchHat} className='w-24 md:w-32 lg:w-48 witch-hat flicker-in-1 transition-all ease-in'/>
+                        <img src={witchHat} className='hidden lg:block w-24 md:w-32 lg:w-48 witch-hat flicker-in-1 transition-all ease-in'/>
                     </div>
                     <div>
-                        <h1 className="text-orange-600 font-bold uppercase text-4xl">menu</h1>
+                        <h1 className="text-orange-600 font-bold uppercase text-3xl">menu</h1>
                     </div>
-                    <div className='relative'>
+                    <div className='relative mt-12 lg:mt-0'>
                         <label htmlFor='search-box' className='absolute left-3 top-1.5 text-white'><img src={search} className='search-img'/></label>
-                        <input id='search-box' className="h-9 py-2 px-10 w-72 rounded-full border-2 border-orange-600 text-orange-600 bg-transparent placeholder:text-orange-600 placeholder:uppercase" placeholder="Search"></input>
+                        <input id='search-box' className="h-9 py-2 px-10 w-92 lg:w-64 rounded-full border-2 border-orange-600 text-orange-600 bg-transparent placeholder:text-orange-600 placeholder:uppercase" placeholder="Search"></input>
                     </div>
                 </div>
             </div>
             <section>
-                <div className='flex justify-center flex-wrap max-w-[1440px] mx-auto'>
+                <div className='flex justify-center flex-wrap max-w-[1440px] mx-auto mb-16'>
                     <div className='flex justify-center items-center flex-wrap'>
                         {menu.map(item => (
-                            <div key={item.id} className='flex self-baseline w-full max-w-[240px] text-center m-3'>
-                                <div className=''>
+                            <div key={item.id} className='flex self-baseline w-full max-w-[240px] text-center m-3 hover-effect basis-[40%]'>
+                                <Link to='/inprogress'>
                                     <div className='relative'>
-                                        <img src={item.image} className='relative z-30 w-full transition-all duration-200 ease-in-out'/>
-                                        <div className='absolute border border-orange-600/30 w-full h-[52%] rounded-xl bottom-4 left-0 z-0'></div>
+                                        <img src={item.image} className='relative z-30 w-full transition-all duration-200 ease-in-out scale-effect'/>
+                                        <div className='absolute border-effect border border-orange-600/25 w-full h-[52%] rounded-xl bottom-3 left-0 z-0 transition-colors ease-linear duration-200'></div>
                                     </div>
-                                    <p className='text-orange-600 uppercase'>{item.menuItem}</p>
-                                    <Link to='/inprogress'></Link>
-                                </div>
+                                    <h3 className='text-orange-600 my-6 uppercase'>{item.menuItem}</h3>          
+                                </Link>
                             </div>
                         ))}
                     </div>
