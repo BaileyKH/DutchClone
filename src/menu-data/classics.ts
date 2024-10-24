a4bb1e9e2ff547e01989dcc1f1140f6936c00c1e
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-24 13:57:49 -0700

    created / added classic coffee data

---
+++ src/menu-data/classics.ts
@@ -0,0 +1,64 @@
+import cookieButter from '/src/assets/images/menu/categories/classics/CookieButterLatteIced-512.webp'
+import mocha from '/src/assets/images/menu/categories/classics/h_ice_GERMAN_CHOCOLATE.webp'
+import latte from '/src/assets/images/menu/categories/classics/h_ice_COOKIE.webp'
+import coldBrew from '/src/assets/images/menu/categories/classics/Iced_Still_Cold_Brew_HERO.png'
+import americano from '/src/assets/images/menu/categories/classics/Iced_Americano_HERO.png'
+import oatMilk from '/src/assets/images/menu/categories/classics/h_ice_lat_oat_WHITE_CHOCOLATE_LAVENDER_512.webp'
+import nitro from '/src/assets/images/menu/categories/classics/Nitro_Cold_Brew_Iced_HERO.png'
+
+export const classicCoffees = {
+    title: "Snacks",
+    description: "Delicious treats to pair with your drink.",
+    items: [
+        {
+            id: 1,
+            itemName: 'cookie butter latte',
+            itemDescription: 'Cookie butter flavor and swirl w/ Soft Top™ and cookie butter drizzle',
+            itemImage: cookieButter,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 2,
+            itemName: 'mocha',
+            itemDescription: 'Espresso, Chocolate Milk',
+            itemImage: mocha,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 3,
+            itemName: 'latte',
+            itemDescription: 'Espresso, Milk',
+            itemImage: latte,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 4,
+            itemName: 'cold brew',
+            itemDescription: 'Refreshing & Bold',
+            itemImage: coldBrew,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 5,
+            itemName: 'americano',
+            itemDescription: 'Espresso and Water',
+            itemImage: americano,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 6,
+            itemName: 'oat milk latte',
+            itemDescription: 'Espresso, Oat Milk',
+            itemImage: oatMilk,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 7,
+            itemName: 'nitro',
+            itemDescription: 'Smooth & Creamy',
+            itemImage: nitro,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+
+    ]
+}
