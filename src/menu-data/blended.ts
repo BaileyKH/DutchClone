bcb9ad2cf17f1630d0f48e391759edcbe30ee853
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-24 13:15:48 -0700

    created / added blended items data

---
+++ src/menu-data/blended.ts
@@ -0,0 +1,72 @@
+import goldenEagle from '/src/assets/images/menu/categories/blended/frz_GOLDEN_EAGLE.webp'
+import caramelizer from '/src/assets/images/menu/categories/blended/frz_MOCHA.webp'
+import nineOneOne from '/src/assets/images/menu/categories/blended/rz_ANNIHILATOR_512.webp'
+import cocomo from '/src/assets/images/menu/categories/blended/h_bln_frz_MOCHA_512.webp'
+import annihilator from '/src/assets/images/menu/categories/blended/annil.webp'
+import kicker from '/src/assets/images/menu/categories/blended/kicker.webp'
+import doubleTort from '/src/assets/images/menu/categories/blended/doublet.webp'
+import picturePerf from '/src/assets/images/menu/categories/blended/perfect.webp'
+
+export const blendedFreeze = {
+    title: "blended freeze",
+    description: "Blended coffee perfection paired with your favorite flavor.",
+    items: [
+        {
+            id: 1,
+            itemName: 'golden eagle',
+            itemDescription: 'Blended Vanilla, Caramel Breve w/Caramel Drizzle',
+            itemImage: goldenEagle,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 2,
+            itemName: 'caramelizer',
+            itemDescription: 'Blended Caramel Mocha',
+            itemImage: caramelizer,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 3,
+            itemName: '9-1-1',
+            itemDescription: '6-shot Irish Cream Breve',
+            itemImage: nineOneOne,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 4,
+            itemName: 'cocomo',
+            itemDescription: 'Blended Coconut Mocha',
+            itemImage: cocomo,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 5,
+            itemName: 'annihilator',
+            itemDescription: 'Chocolate Macadamia Nut Breve',
+            itemImage: annihilator,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 6,
+            itemName: 'kicker',
+            itemDescription: 'Irish Cream Breve',
+            itemImage: kicker,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 7,
+            itemName: 'double torture',
+            itemDescription: 'Extra Double Shot Vanilla Mocha',
+            itemImage: doubleTort,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 8,
+            itemName: 'picture perfect',
+            itemDescription: 'Caramel & Chocolate Drizzle',
+            itemImage: picturePerf,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+    ]
+
+}
