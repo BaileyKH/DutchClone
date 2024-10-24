fa8a0c78b7ad13caa0fdbdac79f4d898356fc872
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-24 13:34:45 -0700

    created / added snacks data

---
+++ src/menu-data/snacks.ts
@@ -0,0 +1,41 @@
+import chocoChip from '/src/assets/images/menu/categories/snacks/Choc_chp_muf_top_512.webp'
+import lemonPop from '/src/assets/images/menu/categories/snacks/Lemon_Pop_muf_top_512.webp'
+import orangeCran from '/src/assets/images/menu/categories/snacks/Orange_Cran_muf_top_512.webp'
+import granola from '/src/assets/images/menu/categories/snacks/Granola_bar_512.webp'
+
+export const snackFood = {
+    title: "Snacks",
+    description: "Delicious treats to pair with your drink.",
+    items: [
+        {
+            id: 1,
+            itemName: 'chocolate chip muffin top',
+            itemDescription: 'Chocolate Chip Muffin Top',
+            itemImage: chocoChip,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 2,
+            itemName: 'lemon poppyseed muffin top',
+            itemDescription: '',
+            itemImage: lemonPop,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 3,
+            itemName: 'orange cranberry muffin top',
+            itemDescription: '',
+            itemImage: orangeCran,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+        {
+            id: 4,
+            itemName: 'granola bar',
+            itemDescription: '',
+            itemImage: granola,
+            itemPath: 'caramel-pumpkin-brulee-breve'
+        },
+
+    ]
+
+}
