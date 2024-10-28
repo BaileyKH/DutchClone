951d786112488d6b67fbfe13af02912c122b7e01
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-28 13:04:03 -0700

    added data for new seasonal drinks

--- src/menu-data/seasonal-drinks.ts
+++ src/menu-data/seasonal-drinks.ts
@@ -1,6 +1,8 @@
 import caramelPumpkin from '/src/assets/images/menu/categories/seasonal/CaramelPumpkinBruleeBreve.webp'
 import cookieButter from '/src/assets/images/menu/categories/seasonal/CookieButterLatteIced.webp'
 import caramelApple from '/src/assets/images/menu/categories/seasonal/CaramelAppleIcedRebel.webp'
+import vampireSlayer from '/src/assets/images/menu/categories/seasonal/vampire-slayer.webp'
+import zombieChai from '/src/assets/images/menu/categories/seasonal/h_ice_cha_VANILLA_512.webp'
 
 export const seasonalDrinks = {
     title: "seasonal drinks",
@@ -26,6 +28,20 @@ export const seasonalDrinks = {
             itemDescription: 'Green apple & salted caramel energy drink w/ green apple & salted caramel float',
             itemImage: caramelApple,
             itemPath: 'caramel-apple-rebel'
+        },
+        {
+            id: 4,
+            itemName: 'Vampire Slayer Rebel',
+            itemDescription: 'Pomegranate & Strawberry energy drink',
+            itemImage: vampireSlayer,
+            itemPath: 'vampire-slayer'
+        },
+        {
+            id: 5,
+            itemName: 'White Zombie Chai',
+            itemDescription: 'White chocolate & vanila',
+            itemImage: zombieChai,
+            itemPath: 'white-zombie-chai'
         },
     ]
 
